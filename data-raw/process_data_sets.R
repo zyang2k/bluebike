017fc8db5141db747b9d34004c7e4389e5cdbd88
zyang2k <zyang2k@gmail.com>
2022-03-30 21:12:29 -0400

    removed large .csv files fetch Feb. 2022 data from a created GitHub repo

--- data-raw/process_data_sets.R
+++ data-raw/process_data_sets.R
@@ -8,15 +8,14 @@ library(usethis)
 
 # Blue Bikes Comprehensive Trip Histories 2020-2022
 
-trip_history <- list.files("data-raw", pattern = "*.csv", full.names = TRUE)%>%
-  map_df(~read_csv(file = .x))
+# 02/2022 data from "https://raw.githubusercontent.com/zyang2k/Blue-Bike-Trip-History-Data/master/202202-bluebikes-tripdata.csv"
+trip_history_202202 <-
+  "https://raw.githubusercontent.com/zyang2k/Blue-Bike-Trip-History-Data/master/202202-bluebikes-tripdata.csv" %>%
+  readr::read_csv()
 
-# Do not try to run this block unless necessary!
-usethis::use_data(trip_history, overwrite = TRUE)
-
-# Run this instead
+# Run this to retrieve a sample of 1000 rows
 set.seed(2022)
-trip_history_sample <- trip_history %>%
+trip_history_sample <- trip_history_202202 %>%
   sample_n(1000)
 usethis::use_data(trip_history_sample, overwrite = TRUE)
 
