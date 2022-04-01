000449527d146be5ac1153de0a0925d8d46f6259
zyang2k <zyang2k@gmail.com>
2022-04-01 11:11:56 -0400

    updated with documentations of dataset/pkg with new var names

--- R/trip_history_sample.R
+++ R/trip_history_sample.R
@@ -5,20 +5,20 @@
 #'
 #' @format A data frame of 1,000 rows representing each sample of trip history
 #' \describe{
-#'   \item{tripduration}{Trip duration of each trip measured in seconds}
-#'   \item{starttime}{Start time and date of each trip}
-#'   \item{stoptime}{Stop time and date of each trip}
-#'   \item{start station id}{The identification variable of the start station}
-#'   \item{start station name}{The name of the end station}
-#'   \item{start station latitude}{The latitude of the start station}
-#'   \item{start station longitude}{The longitude of the start station}
-#'   \item{end station id}{The identification variable of the end station}
-#'   \item{end station name}{The name of the end station}
-#'   \item{end station latitude}{The latitude of the end station}
-#'   \item{end station longitude}{The longitude of the start station}
-#'   \item{bikeid}{The identification variable of the bike corresponding to each trip}
-#'   \item{usertype}{Type of user in each trip (Casual = Single Trip or Day Pass user; Member = Annual or Monthly Member)}
-#'   \item{postal code}{Postal code of the user}
+#'   \item{trip_duration}{Trip duration of each trip measured in seconds}
+#'   \item{start_time}{Start time and date of each trip}
+#'   \item{stop_time}{Stop time and date of each trip}
+#'   \item{start_station_id}{The identification variable of the start station}
+#'   \item{start_station_name}{The name of the end station}
+#'   \item{start_station_latitude}{The latitude of the start station}
+#'   \item{start_station_longitude}{The longitude of the start station}
+#'   \item{end_station_id}{The identification variable of the end station}
+#'   \item{end_station_name}{The name of the end station}
+#'   \item{end_station_latitude}{The latitude of the end station}
+#'   \item{end_station_longitude}{The longitude of the start station}
+#'   \item{bike_id}{The identification variable of the bike corresponding to each trip}
+#'   \item{user_type}{Type of user in each trip (Casual = Single Trip or Day Pass user; Member = Annual or Monthly Member)}
+#'   \item{postal_code}{Postal code of the user}
 #' }
 #' @source The original source of the data are bluebikes system data retrieved from <https://www.bluebikes.com/system-data>
 "trip_history_sample"
