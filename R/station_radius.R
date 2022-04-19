1d1f3ad21cf1d28578454adeefc9fb21c1787e52
tianshu-zhang <tzhang26@smith.edu>
2022-04-19 12:05:27 -0400

    station_data function updated

--- R/station_radius.R
+++ R/station_radius.R
@@ -4,12 +4,15 @@
 
 station_radius <- function(long, lat, r){
   station_distances <- station_distance(long,lat) %>%
-    filter(distance < r)
+    filter(as.numeric(distance) < r) %>%
+    left_join(station_data, by = c("station_ID" = "number"))
+
   station_map <- leaflet(data = station_distances) %>%
     addTiles() %>%
-    addCircleMarkers(lng = station_distances$long,
-                     lat = station_distances$,
-                     radius = ,
-                     color = "blue")
-
+    addPopups(station_distances$longitude, station_distances$latitude,
+              popup = paste0("Station: ", station_distances$name, "<br>",
+                             "# docks: ", station_distances$total_docks),
+              options = popupOptions(closeButton = TRUE)) %>%
+    addCircles(station_distances$longitude, station_distances$latitude,
+               radius = r)
 }
