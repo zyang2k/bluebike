fbaa254a328c85152ed893ba8a6ccfebf9cdd9ad
tianshu-zhang <tzhang26@smith.edu>
2022-04-26 23:32:35 -0400

    updated trip_distance warnings and tests

--- R/trip_distance.R
+++ R/trip_distance.R
@@ -19,6 +19,12 @@ globalVariables(c(
 #' sample_distance <- trip_distance(trip_history_sample)$distance
 #' }
 trip_distance <- function(data){
+  if(length(intersect(class(data), c("tbl_df", "tbl", "data.frame"))) == 0){
+    stop("Input should be a tbl_df, tbl, or data.frame object.")
+  }
+  if(length(intersect(c("start_station_longitude", "start_station_latitude", "end_station_longitude", "end_station_latitude"), colnames(data))) != 4){
+    stop("Invalid data. Need longitude and latitude of start and end stations to compute distance.")
+  }
   data_sf <- data %>%
     mutate(start_station_sf = st_as_sf(trip_history_sample, coords = c("start_station_longitude", "start_station_latitude"),
                                        crs = 4326, agr = "constant"),
